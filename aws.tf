98c2eed2350e7fbe9c8d71923db18785b3b71a6d
村上恭哉 <mkkyyyy@isa3-dhcp-190-214.naist.jp>
2022-03-17 01:16:42 +0900

    4.6

--- aws.tf
+++ aws.tf
@@ -1,18 +1,18 @@
 locals {
-    availability_zone_ids = [
-        "apne1-az1",
-        "apne1-az2",
-        "apne1-az4",
-    ]
+  availability_zone_ids = [
+    "apne1-az1",
+    "apne1-az2",
+    "apne1-az4",
+  ]
 }
 
 provider "aws" {
-    region = "ap-northeast-1"
+  region = "ap-northeast-1"
 }
 
 # zone idからそのzoneの情報を取得する
 # zone_id = apne1-az1はap-northeast-1aのこと
-data "aws_availability_zone" "availables"{
-    count = length(local.availability_zone_ids)
-    zone_id = local.availability_zone_ids[count.index]
+data "aws_availability_zone" "availables" {
+  count   = length(local.availability_zone_ids)
+  zone_id = local.availability_zone_ids[count.index]
 }
--- network.tf
+++ network.tf
@@ -1,52 +1,96 @@
 resource "aws_vpc" "sample" {
-    cidr_block           = "10.0.0.0/16"
-    enable_dns_support   = true
-    enable_dns_hostnames = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-    tags = {
-        Name = "sample"
-    }
+  tags = {
+    Name = "sample"
+  }
 }
 
 resource "aws_subnet" "sample_public" {
-    count = length(local.availability_zone_ids)
-    vpc_id = aws_vpc.sample.id
+  count  = length(local.availability_zone_ids)
+  vpc_id = aws_vpc.sample.id
 
-    cidr_block = cidrsubnet(aws_vpc.sample.cidr_block, 3, 0 + count.index*2)
-    availability_zone = data.aws_availability_zone.availables[count.index].name
+  cidr_block        = cidrsubnet(aws_vpc.sample.cidr_block, 3, 0 + count.index * 2)
+  availability_zone = data.aws_availability_zone.availables[count.index].name
 
-    tags = {
-        Name = "sample-public-subnet-${data.aws_availability_zone.availables[count.index].name_suffix}"
-    }
+  tags = {
+    Name = "sample-public-subnet-${data.aws_availability_zone.availables[count.index].name_suffix}"
+  }
 }
 
 resource "aws_subnet" "sample_private" {
-    count = length(local.availability_zone_ids)
-    vpc_id = aws_vpc.sample.id
+  count  = length(local.availability_zone_ids)
+  vpc_id = aws_vpc.sample.id
 
-    cidr_block = cidrsubnet(aws_vpc.sample.cidr_block, 3, 1 + 2*count.index)
-    availability_zone = data.aws_availability_zone.availables[count.index].name
+  cidr_block        = cidrsubnet(aws_vpc.sample.cidr_block, 3, 1 + 2 * count.index)
+  availability_zone = data.aws_availability_zone.availables[count.index].name
 
-    tags = {
-        Name = "sample-private-subnet-${data.aws_availability_zone.availables[count.index].name_suffix}"
-    }
+  tags = {
+    Name = "sample-private-subnet-${data.aws_availability_zone.availables[count.index].name_suffix}"
+  }
 }
 
-#resource "aws_internet_gateway" "sample_public_1a_igw" {
-#    vpc_id = aws_vpc.sample.id
-#}
-#
-#resource "aws_route_table" "sample_public_1a" {
-#    vpc_id = aws_vpc.sample.id
-#}
-#
-#resource "aws_route" "sample_public_1a" {
-#    route_table_id = aws_route_table.sample_public_1a.id
-#    gateway_id = aws_internet_gateway.sample_public_1a_igw.id
-#    destination_cidr_block = "0.0.0.0/0"
-#}
-#
-#resource "aws_route_table_association" "sample_public_1a" {
-#    subnet_id = aws_subnet.sample_public_1a.id
-#    route_table_id = aws_route_table.sample_public_1a.id
-#}
+resource "aws_internet_gateway" "sample_igw" {
+  vpc_id = aws_vpc.sample.id
+
+  tags = {
+    Name = "sample-igw"
+  }
+}
+
+resource "aws_eip" "sample_eip" {
+  count = length(local.availability_zone_ids)
+  vpc   = true
+  tags = {
+    Name = "sample-eip-${data.aws_availability_zone.availables[count.index].name_suffix}"
+  }
+}
+
+resource "aws_nat_gateway" "sample_ngw" {
+  count         = length(local.availability_zone_ids)
+  subnet_id     = aws_subnet.sample_private[count.index].id
+  allocation_id = aws_eip.sample_eip[count.index].id
+
+  tags = {
+    Name = "sample-ngw-${data.aws_availability_zone.availables[count.index].name_suffix}"
+  }
+
+  depends_on = [aws_internet_gateway.sample_igw]
+}
+
+resource "aws_route_table" "sample_public" {
+  vpc_id = aws_vpc.sample.id
+}
+
+resource "aws_route" "sample_public" {
+  route_table_id = aws_route_table.sample_public.id
+  gateway_id = aws_internet_gateway.sample_igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "sample_public" {
+  count         = length(local.availability_zone_ids)
+  subnet_id = aws_subnet.sample_public[count.index].id
+  route_table_id = aws_route_table.sample_public.id
+}
+
+resource "aws_route_table" "sample_private" {
+  count         = length(local.availability_zone_ids)
+  vpc_id = aws_vpc.sample.id
+}
+
+resource "aws_route" "sample_private" {
+  count         = length(local.availability_zone_ids)
+  route_table_id = aws_route_table.sample_private[count.index].id
+  nat_gateway_id = aws_nat_gateway.sample_ngw[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+
+resource "aws_route_table_association" "sample_private" {
+  count = length(local.availability_zone_ids)
+  subnet_id = aws_subnet.sample_private[count.index].id
+  route_table_id = aws_route_table.sample_private[count.index].id
+}
